bd072039bd06c76125b436432df696c0d39738b0
Mihaela Schibinschi <solonarumihaela@gmail.com>
2026-01-16 11:46:35 +0200

    edited user_Data.sh script

--- Assignment 8 - FINAL BOSS/terraform/infra/user_data.sh
+++ Assignment 8 - FINAL BOSS/terraform/infra/user_data.sh
@@ -6,28 +6,19 @@ apt-get install -y docker.io unzip curl
 
 systemctl start docker
 systemctl enable docker
+
 usermod -aG docker ubuntu
 
-# Install Docker Compose v2
-curl -L "https://github.com/docker/compose/releases/download/v2.27.0/docker-compose-linux-x86_64" \
-  -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+mkdir -p /usr/local/lib/docker/cli-plugins
+curl -SL https://github.com/docker/compose/releases/download/v2.27.0/docker-compose-linux-x86_64 \
+  -o /usr/local/lib/docker/cli-plugins/docker-compose
+chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
 
-# Install AWS CLI
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o awscliv2.zip
 unzip -q awscliv2.zip
 ./aws/install
 
-# Login to ECR
-aws ecr get-login-password --region ${AWS_REGION} \
-  | docker login \
-    --username AWS \
-    --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com
-
-# Create docker-compose.yml
 cat <<EOF > /home/ubuntu/docker-compose.yml
-version: "3.8"
-
 services:
   backend:
     image: ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/ip-spectre-backend:latest
@@ -43,8 +34,9 @@ services:
 EOF
 
 chown ubuntu:ubuntu /home/ubuntu/docker-compose.yml
-cd /home/ubuntu
 
-sleep 30
-docker compose pull
-docker compose up -d
+su - ubuntu -c "aws ecr get-login-password --region ${AWS_REGION} \
+  | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com"
+
+su - ubuntu -c "cd /home/ubuntu && docker compose pull"
+su - ubuntu -c "cd /home/ubuntu && docker compose up -d"
