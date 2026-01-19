2ce4c0dd45d78a901b81e08e030ec5f78c22a677
Mihaela Schibinschi <solonarumihaela@gmail.com>
2026-01-19 10:20:13 +0200

    clean the repo & prepare for finish

--- Assignment 8 - FINAL BOSS/terraform/infra/iam.tf
+++ Assignment 8 - FINAL BOSS/terraform/infra/iam.tf
@@ -22,7 +22,28 @@ resource "aws_iam_policy" "ip_spectre_dynamodb" {
   })
 }
 
-# resource "aws_iam_role_policy_attachment" "cloudwatch_logs" {
-#   role       = aws_iam_role.ec2_role.name
-#   policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
-# }
+resource "aws_iam_policy" "cloudwatch_logs" {
+  name        = "${var.project_name}-cloudwatch-logs-policy"
+  description = "Allow EC2 to write logs to CloudWatch"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+          "logs:DescribeLogStreams"
+        ]
+        Resource = "arn:aws:logs:${var.aws_region}:${var.aws_account_id}:log-group:/ip-spectre/*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "cloudwatch_logs" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = aws_iam_policy.cloudwatch_logs.arn
+}
